74baba55ac7826217f27300045fb09092a686079
Rafaela <rn-15@hotmail.com>
2021-08-02 08:41:44 -0300

    Possíveis cenarios cobertos por testes

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,13 +7,28 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteService {
 
+    private static final BigDecimal TRES_PORCENTO = new BigDecimal("0.03");
+    private static final BigDecimal QUINZE_PORCENTO = new BigDecimal("0.15");
+    private static final BigDecimal VINTE_PORCENTO = new BigDecimal("0.20");
+
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if(desempenho.equals(Desempenho.A_DESEJAR)) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+        if (desempenho.equals(Desempenho.A_DESEJAR)) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(TRES_PORCENTO);
 
             funcionario.reajustarSalario(reajuste);
         }
 
+        if (desempenho.equals(Desempenho.BOM)) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(QUINZE_PORCENTO);
+
+            funcionario.reajustarSalario(reajuste);
+        }
+
+        if (desempenho.equals(Desempenho.OTIMO)) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(VINTE_PORCENTO);
+
+            funcionario.reajustarSalario(reajuste);
+        }
     }
 
 }
--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -10,17 +10,64 @@ import org.junit.jupiter.api.Test;
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
+
+/**
+ * O sistema deve permitir que os funcionários recebem reajuste salarial anual baseado em seu
+ * desempenho, obedecendo as seguintes regras:
+ * 1. Se o desempenho foi "A desejar", reajuste será de apenas 3% do salário
+ * 2. Se o desempenho foi "Bom", reajuste será de 15% do salário
+ * 3. Se o desempenho foi "Ótimo", reajuste será de 20% do salário
+ */
 public class ReajusteServiceTest {
     
-
     @Test
     public void reajusteDeveriaSerTresPorcentoQuandoReajusteForADesejar() {
+        BigDecimal tresPorcento = new BigDecimal("0.03");
+        BigDecimal salarioAtual = new BigDecimal("3000");        
+        BigDecimal valorAcrescentado = salarioAtual.multiply(tresPorcento);
+        BigDecimal salarioEsperado = new BigDecimal("0.00");
+        salarioEsperado = (salarioAtual.add(valorAcrescentado));
+        
         ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000") );
+        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), salarioAtual);
 
         service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
 
-        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+        assertEquals(salarioEsperado, funcionario.getSalario());
+
+    }
+
+    @Test
+    public void reajusteDeveriaSerTresPorcentoQuandoReajusteForBom() {
+        BigDecimal tresPorcento = new BigDecimal("0.15");
+        BigDecimal salarioAtual = new BigDecimal("5000");        
+        BigDecimal valorAcrescentado = salarioAtual.multiply(tresPorcento);
+        BigDecimal salarioEsperado = new BigDecimal("0.00");
+        salarioEsperado = (salarioAtual.add(valorAcrescentado));
+        
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Rogério", LocalDate.now(), salarioAtual);
+
+        service.concederReajuste(funcionario, Desempenho.BOM);
+
+        assertEquals(salarioEsperado, funcionario.getSalario());
+
+    }
+
+    @Test
+    public void reajusteDeveriaSerTresPorcentoQuandoReajusteForOtimo() {
+        BigDecimal tresPorcento = new BigDecimal("0.20");
+        BigDecimal salarioAtual = new BigDecimal("3000");        
+        BigDecimal valorAcrescentado = salarioAtual.multiply(tresPorcento);
+        BigDecimal salarioEsperado = new BigDecimal("0.00");
+        salarioEsperado = (salarioAtual.add(valorAcrescentado));
+        
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Rogério", LocalDate.now(), salarioAtual);
+
+        service.concederReajuste(funcionario, Desempenho.OTIMO);
+
+        assertEquals(salarioEsperado, funcionario.getSalario());
 
     }
 }
